2c539799444e51f8e42bbbe7449a1f5fe3f55a9e
Alexis Yushin <alexis@apexive.com>
2025-01-28 20:27:39 -0400

    postgres tuning

--- linode/odoo/ansible.tf
+++ linode/odoo/ansible.tf
@@ -21,7 +21,25 @@ resource "local_file" "ansible_host_vars" {
 
     postgres_enabled: true
     postgres_connection_password: 'Ceocoo0aaPhe5yee7sah2eeh6vah2ok8Sofohch2rien3phie3aineeJaiquevai'
-    postgres_process_extra_arguments: []
+    postgres_postgres_process_extra_arguments_auto: [
+      "-c 'max_connections=1000'",
+      "-c 'shared_buffers=8192MB'",
+      "-c 'effective_cache_size=24576MB'",
+      "-c 'maintenance_work_mem=2048MB'",
+      "-c 'checkpoint_completion_target=0.9'",
+      "-c 'wal_buffers=16MB'",
+      "-c 'default_statistics_target=100'",
+      "-c 'random_page_cost=1.1'",
+      "-c 'effective_io_concurrency=200'",
+      "-c 'work_mem=24576kB'",
+      "-c 'huge_pages=try'",
+      "-c 'min_wal_size=80MB'",
+      "-c 'max_wal_size=1024MB'",
+      "-c 'max_worker_processes=16'",
+      "-c 'max_parallel_workers=12'",
+      "-c 'max_parallel_workers_per_gather=6'",
+      "-c 'max_parallel_maintenance_workers=4'"
+    ]
 
     # Traefik configuration
     mash_playbook_reverse_proxy_type: playbook-managed-traefik
@@ -74,7 +92,7 @@ resource "local_file" "ansible_host_vars" {
       max_cron_threads = 1
       proxy_mode = True
       server_wide_modules = web,dbfilter_from_header
-      workers = 4
+      workers = 25
   EOT
 
   depends_on = [linode_instance.odoo]
